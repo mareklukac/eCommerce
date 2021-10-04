233ee39547bc5deba4228916d5467efc0339fcc5
Marek Lukac <marek.lukac08@gmail.com>
2021-10-04 22:40:53 +0200

    header navbar using grid

--- e-commerce-app/src/components/header/Header.js
+++ e-commerce-app/src/components/header/Header.js
@@ -1,31 +1,40 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { Container, Row, Col } from "react-bootstrap";
+
 const Header = () => {
   return (
     <nav className={styles.topNavbar}>
-      <ul className={styles.leftNav}>
-        <span></span>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">News</a>
-        </li>
-        <li>
-          <a href="/">Contact</a>
-        </li>
-        <li>
-          <a href="/">About</a>
-        </li>
-      </ul>
-      <ul className={styles.rightNav}>
-        <li>
-          <a href="/">Login</a>
-        </li>
-        <li>
-          <a href="/">Sign Up</a>
-        </li>
-      </ul>
+      <Container fluid>
+        <Row>
+          <Col md={8} sm={6}>
+            <ul className={styles.ulLeftNav}>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">News</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={4} sm={6}>
+            <ul className={styles.ulRightNav}>
+              <li>
+                <a href="/">Login</a>
+              </li>
+              <li>
+                <a href="/">Sign Up</a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </nav>
   );
 };
